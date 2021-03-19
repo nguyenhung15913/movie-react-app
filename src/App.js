d6b0dcc56eaa7896e1ef27458b6e81f0d21a1ea4
Hung <69558780+nguyenhung15913@users.noreply.github.com>
2021-03-19 13:57:38 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import Home from './Home'
+import Navigation from './Navigation'
+import Listing from './Listing'
+import Footer from './Footer'
+import Movie from './Movie'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+function App() {
+	return (
+		<div className="App">
+			<Navigation />
+
+			<Switch>
+				<Route exact path="/" render={() => <Home />} />
+
+				<Route path="/movies" render={() => <Listing />} />
+
+				<Route
+					path="/movie/:id"
+					render={(props) => <Movie id={props.match.params.id} />}
+				/>
+				<Route path="/movie/undefined" render={() => <Redirect to="/" />} />
+			</Switch>
+
+			<Footer />
+		</div>
+	)
+}
+
+export default App
